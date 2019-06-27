ebf14f8c3b7a60e3d65b7c617c8fd6dbce921cd7
adamswonderboy <daedrawonder@outlook.com>
2019-06-27 19:37:42 +0300

    initialised repo

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+var myVar;
+   function myFunction() {
+       myVar = setTimeout(showPage, 1000);
+   }
+   function showPage() {
+     document.getElementById("loader").style.display = "none";
+     document.getElementById("page").style.display = "block";
+     document.getElementById("j").style.background= "white";
+   }
